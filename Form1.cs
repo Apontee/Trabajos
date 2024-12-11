7e48b9f3337439c310766f6279209b542a194079
Apontee <136099437+Apontee@users.noreply.github.com>
2024-12-11 16:05:12 -0500

    Trabajo

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionTareasPendientes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                                                                                
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTarea_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTarea.Text))
+            {
+                // Agregar la tarea al ListBox
+                lstTareas.Items.Add(txtTarea.Text);
+                // Limpiar el TextBox
+                txtTarea.Clear();
+                txtTarea.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese una tarea.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEliminarTarea_Click(object sender, EventArgs e)
+        {
+            if (lstTareas.SelectedItem != null)
+            {
+                lstTareas.Items.Remove(lstTareas.SelectedItem);
+                MessageBox.Show("La tarea se ha eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione una tarea para eliminar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+    }
+
+        private void btnLimpiarLista_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("¿Está seguro de que desea eliminar todas las tareas?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmResult == DialogResult.Yes)
+            {
+                lstTareas.Items.Clear();
+            }
+        }
+    }
+}
